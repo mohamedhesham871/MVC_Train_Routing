3750be9dc03156f86cbf232c34aec8121137999d
20100 <mmmelkady23@gmail.com>
2025-03-27 05:57:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace MVC_Train_Routing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+
+            var app = builder.Build();
+
+
+            app.MapGet("/", () => "Hello World!");
+
+
+            //app.MapGet(pattern: "/CEO/{ID}", requestDelegate: async context =>
+            //{
+            //    //First way to Route 
+            //    var name /* = context.GetRouteValue("name")*/
+            //    //second way ..              // it's indexer
+            //              = context.Request.RouteValues["ID"];
+            //    await context.Response.WriteAsync($"hello {name}");
+            //});
+           
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action}/{id:int?}/{name:alpha}",
+                defaults: new { Controller = "Movie", action = "GETMovie" },
+              //  constraints: new { id = new IntRouteConstraint() }
+              constraints :new {id =@"\d{2}"});
+
+            app.Run();
+        }
+    }
+}
---
+++ models/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Train_Routing.models
+{
+    public class MovieController:Controller
+    {
+        public string? name { get; set; }
+        public int ID  { get; set; }
+        public  string ?Title { get; set; }
+        public int Code  { get; set; }
+
+
+        //First Action 
+        
+        public ContentResult  GETMovie(int id,string name)
+        {
+            //return $"Movie ****** {id} and name is {name}";
+            //   ContentResult result = new ContentResult();
+            // result.Content = $"Movie ****** {id} and name is {name}";
+            // result.StatusCode = 900;
+            return Content($"Movie ****** {id} and name is {name}");
+           // return result;
+        }
+        //To excute Any action =>> http://{Base URL }/{Controller name}/{Action Name}/
+    }
+}
